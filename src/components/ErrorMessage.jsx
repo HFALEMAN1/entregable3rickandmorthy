715f1dd40d353b247568b1bb78f9f61cd14044f4
Hector Falcon <hector18falcon@hotmail.com>
2022-12-11 19:44:49 -0600

    imgportalurl

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -6,8 +6,8 @@ const ErrorMessage = () => {
 
             <div className='imgportal' >
 
-                <img className='img--color' src="./src/assets/img/portalrickandmorty.png" alt="" />
-
+                {/* <img className='img--color' src="./src/assets/img/portalrickandmorty.png" alt="" /> */}
+                <img className='img--color' src="https://i.pinimg.com/originals/98/29/21/9829215db6f9210c0ae4e318e854cb1f.png" alt="" />
             </div>
 
             <p>Not found this dimension </p>
